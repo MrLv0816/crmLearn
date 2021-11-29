19905d99d813b3fb20b0ab66e576c54766e174c7
MrLv <lvsheng1314@foxmail.com>
2021-11-29 22:27:21 +0800

    First Commit

---
+++ src/main/java/cn/konfan/crm/interceptor/LoginInterceptor.java
@@ -0,0 +1,53 @@
+package cn.konfan.crm.interceptor;
+
+import org.omg.PortableInterceptor.Interceptor;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author MrLv
+ * @date 2021/11/29
+ * @apiNote
+ */
+public class LoginInterceptor implements HandlerInterceptor {
+    /**
+     * 控制器执行前的拦截操作
+     * @param httpServletRequest
+     * @param httpServletResponse
+     * @param o
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        return true;
+    }
+
+    /**
+     * 控制器执行后的操作
+     * @param httpServletRequest
+     * @param httpServletResponse
+     * @param o
+     * @param modelAndView
+     * @throws Exception
+     */
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+    /**
+     * 页面加载完成前的操作
+     * @param httpServletRequest
+     * @param httpServletResponse
+     * @param o
+     * @param e
+     * @throws Exception
+     */
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
